729406e3051c223325b5aa47f3fbb2a80bc72cce
tjellis19 <54557118+tjellis19@users.noreply.github.com>
2019-09-18 19:06:55 -0400

    Add files via upload

---
+++ Fan.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_review;
+
+/**
+ *
+ * @author tjell
+ */
+public class Fan {
+    //----Variables----\\
+    final int SLOW = 1;
+    final int MEDIUM = 2;
+    final int FAST = 3;
+    private int speed;
+    private boolean on;
+    private double radius;
+    String color;
+    
+    public Fan()
+    {
+        
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+    public String toString()
+    {
+        return "";
+    }
+}
