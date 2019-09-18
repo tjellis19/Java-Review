cb28dc078b0d9b6562d1cb6aa0ea5f48bf9352e5
tjellis19 <54557118+tjellis19@users.noreply.github.com>
2019-09-18 19:46:01 -0400

    Add files via upload
    Made two fan objects in the main class. 
    Assigned each value accordingly. 
    Completed assignment.

--- Java_Review.java
+++ Java_Review.java
@@ -16,6 +16,22 @@ public class Java_Review {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Fan fan1 = new Fan();
+        Fan fan2 = new Fan();
+        
+        fan1.setSpeed(fan1.FAST);
+        fan1.setRadius(10);
+        fan1.setColor("yellow");
+        fan1.setOn(true);
+        System.out.println(fan1.toString() + "\n");
+        
+        fan2.setSpeed(fan2.MEDIUM);
+        fan2.setRadius(5);
+        fan2.setColor("blue");
+        fan2.setOn(false);
+        System.out.println(fan2.toString());
+        
+        
     }
     
 }
